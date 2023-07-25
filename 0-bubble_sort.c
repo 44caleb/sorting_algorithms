b910555ac3b2270af538b1823c77d2b42342002e
44caleb <calebadinfono2@gmail.com>
2023-07-25 15:48:39 -0700

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, nsize = size;
+    int tmp, arranged = 0;
+
+    while (arranged == 0)
+    {
+        arranged = 1;
+
+        if (nsize == 0)
+        {
+            break;
+        }
+        for (i = 0; i < nsize - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+                print_array(array, size);
+                arranged = 0;
+            }
+        }
+        nsize--;
+    }
+}
