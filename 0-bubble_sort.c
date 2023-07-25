721a58074d2b9ea80ef5831437ef66c47dc7ee69
44caleb <calebadinfono2@gmail.com>
2023-07-25 16:06:36 -0700

    bubble betty fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,35 @@
 #include "sort.h"
 
-
+/**
+ * bubble_sort - sorts with bubble sort
+ * @array: array of elements
+ * @size: size of array
+ * Return: nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, nsize = size;
-    int tmp, arranged = 0;
+	size_t i, nsize = size;
+	int tmp, arranged = 0;
 
-    while (arranged == 0)
-    {
-        arranged = 1;
+	while (arranged == 0)
+	{
+		arranged = 1;
 
-        if (nsize == 0)
-        {
-            break;
-        }
-        for (i = 0; i < nsize - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                print_array(array, size);
-                arranged = 0;
-            }
-        }
-        nsize--;
-    }
+		if (nsize == 0)
+		{
+			break;
+		}
+		for (i = 0; i < nsize - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				arranged = 0;
+			}
+		}
+		nsize--;
+	}
 }
--- sort.h
+++ sort.h
@@ -5,78 +5,73 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-
 /**
-* struct listint_s - Doubly linked list node
-*
-* @n: Integer stored in the node
-* @prev: Pointer to the previous element of the list
-* @next: Pointer to the next element of the list
-*/
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
- const int n;
- struct listint_s *prev;
- struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-
-
-
 /**
-* print_list - Prints a list of integers
-*
-* @list: The list to be printed
-*/
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
 void print_list(const listint_t *list)
 {
- int i;
- i = 0;
- while (list)
- {
- if (i > 0)
- printf(", ");
- printf("%d", list->n);
- ++i;
- list = list->next;
- }
- printf("\n");
+	int i;
+
+	i = 0;
+
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
 }
-
-
 
 /**
-* create_listint - Creates a doubly linked list from an array of integers
-*
-* @array: Array to convert to a doubly linked list
-* @size: Size of the array
-*
-* Return: Pointer to the first element of the created list. NULL on failure
-*/
+ * create_listint - Creates a doubly linked list from an array of integers
+ *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
+ *
+ * Return: Pointer to the first element of the created list. NULL on failure
+ */
 listint_t *create_listint(const int *array, size_t size)
 {
- listint_t *list;
- listint_t *node;
- int *tmp;
- list = NULL;
- while (size--)
- {
- node = malloc(sizeof(*node));
- if (!node)
- return (NULL);
- tmp = (int *)&node->n;
- *tmp = array[size];
- node->next = list;
- node->prev = NULL;
- list = node;
- if (list->next)
- list->next->prev = list;
- }
- return (list);
+	listint_t *list;
+	listint_t *node;
+	int *tmp;
+
+	list = NULL;
+
+	while (size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+			return (NULL);
+		tmp = (int *)&node->n;
+		*tmp = array[size];
+		node->next = list;
+		node->prev = NULL;
+		list = node;
+		if (list->next)
+			list->next->prev = list;
+	}
+	return (list);
 }
-
-
-
 
 /**
  * print_array - Prints an array of integers
@@ -86,21 +81,23 @@ listint_t *create_listint(const int *array, size_t size)
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 size_t get_pivot(int *array, size_t start, size_t end, size_t size);
 
 #endif
+
